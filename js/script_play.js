ab87842afd7057b2c94aa113fe12cc8a96a46cb5
Julien <julien.laforge@gmx.fr>
2018-05-18 10:58:26 +0200

    changement redirections html + ajout classes js

---
+++ js/script_play.js
@@ -0,0 +1,25 @@
+class character{
+    constructor (char_name, char_size, char_orientation, char_speed){
+        this.char_name = char_name;
+        this.char_size = char_size;
+        this.char_orientation = char_orientation;
+        this.char_speed = char_speed;
+        // this.char_health = char_health;
+        
+    }
+
+    // char_choice(){
+
+    // }
+}
+
+
+
+class bullet{
+    constructor (bul_size, bul_damages, bul_speed){
+        this.bul_size = bul_size;
+        this.bul_damages = bul_damages;
+        this.bul_speed = bul_speed;
+    }
+}
+
